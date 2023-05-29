b0873344c51fac01a0a67d833f1c0196440d42af
Silte1 <etiennesilue3@gmail.com>
2023-05-29 21:23:50 +0200

    initiate emailJs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useNavigate } from 'react-router'
-// import axios from "axios"
+import emailjs from "@emailjs/browser"
+
 
 
 import "./Contact.css"
@@ -9,41 +10,62 @@ import "./Contact.css"
 export default function Kontakt () {
     const navigate = useNavigate()
     const [email, setEmail] = useState("")
-    const [content, setContent] = useState("")
+    const [message, setMessage] = useState("")
     const [name, setName] = useState("")
     const [showForm, setShowForm] = useState(true)
     const [showSuccessMessage, setShowSuccessMessage] = useState(false)
 
 
-    const postData = async (e) => {
+    const form = useRef()
 
+    const sendEmail = (e) => {
         e.preventDefault();
 
-        try {
-            const response = await fetch(`https://recruiting-game-api.onrender.com/`, {
-
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ sender: email, content: content, name: name }),
-                mode: "no-cors",
-                credentials: "include"
-
-            });
-            if (response.ok) {
-                if (response.status === 200) {
+        emailjs.sendForm('service_uags4zm', 'template_pjscs85', form.current, 'mCvbtzmKAhj6jkJFp')
+            .then((result) => {
+                console.log(result.text);
+                if (result.text === "OK") {
                     setShowForm(false)
                     setShowSuccessMessage(true)
+                    setEmail("")
+                    setName("")
+                    setMessage("")
                 }
-            }
-            console.log(response)
-
-        } catch (error) {
-            console.log(error);
-        }
-
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
     };
+
+    // const postData = async (e) => {
+
+    //     e.preventDefault();
+
+    //     try {
+    //         const response = await fetch(`https://recruiting-game-api.onrender.com/`, {
+
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //             body: JSON.stringify({ sender: email, content: content, name: name }),
+    //             mode: "no-cors",
+    //             credentials: "include"
+
+    //         });
+    //         if (response.ok) {
+    //             if (response.status === 200) {
+    //                 setShowForm(false)
+    //                 setShowSuccessMessage(true)
+    //             }
+    //         }
+    //         console.log(response)
+
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+
+    // };
 
     const handleSucceed = () => {
         setShowSuccessMessage(false)
@@ -54,26 +76,26 @@ export default function Kontakt () {
     return (
         <article className='contactContainer'>
             <section className='containerBody'>
-                {showForm && <form className='contactForm' action="" method="post" onSubmit={postData}>
+                {showForm && <form ref={form} className='contactForm' action="" method="post" onSubmit={sendEmail}>
 
                     <div>
                         <label className='contactFormLabel' htmlFor="">Name</label>
-                        <input type="text" name="name" className='contactFormInput'
+                        <input type="text" name="user_name" className='contactFormInput'
                             placeholder='Gib hier deinen Namen ein'
                             value={name} onChange={(e) => { setName(e.target.value) }} />
                     </div>
                     <div>
                         <label className='contactFormLabel' htmlFor="">E-Mail</label>
-                        <input type="email" name="sender" className='contactFormInput'
+                        <input type="email" name="user_email" className='contactFormInput'
                             required placeholder='Gib hier deine E-Mail-Adresse ein'
                             value={email} onChange={(e) => { setEmail(e.target.value) }} />
                     </div>
 
                     <div>
                         <label className='contactFormLabel' htmlFor="">Deine Nachricht</label>
-                        <textarea name="content" id="" rows="10" className='contactFormMessage' required placeholder='Hallo Etienne, ...'
-                            value={content}
-                            onChange={(e) => { setContent(e.target.value) }}>
+                        <textarea name="message" id="" rows="10" className='contactFormMessage' required placeholder='Hallo Etienne, ...'
+                            value={message}
+                            onChange={(e) => { setMessage(e.target.value) }}>
 
                         </textarea>
                     </div>
