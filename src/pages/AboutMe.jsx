f7749b8ae358914d551815484e6eb808b16dba33
Silte1 <etiennesilue3@gmail.com>
2023-05-30 16:43:02 +0200

    commit

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -8,16 +8,33 @@ export default function AboutMe () {
     return (
         <article className='aboutMeContainer'>
             <section className='textAboutMe'>
-                <p>
-                    Ich heiße Etienne Silué und bin 1995 in Korhogo, einer Stadt im Norden der Elfenbeinküste (Côte d'Ivoire, Westafrika), geboren. Dort ging ich zur Schule und besuchte anschließend die Universität, an der ich mein Bachelor-Studium absolvierte.
-                    2021 heiratete ich und zog deshalb nach Deutschland. Ich wollte die Gelegenheit dieses Neubeginns nutzen, um die berufliche Karriere im Bereich meiner Träume zu starten: Web- und Softwareentwicklung.
-                    Die ersten Monate in Deutschland nutzte ich, um meine Deutsch-Kenntnise zu verbessern und freute mich, im Sommer 2022 eine Umschulung mit DCI (Digital Career Institute) beginnen zu können, um ein Web- und Softwareentwickler zu werden.
-                    Nun bin ich auf der Suche nach einer Arbeitsstelle und freue mich auf diese neue Herausforderung.
-                </p>
-                <p>
-                    Diese Fähigkeiten bringe ich bereits mit:
-                    Frontend (UI/UX) design mit html, css, scss, bootstrap, Javascript, React js; Backend mit Node.js, express js, mongodb(compass, Atlas) / MySql; Versionsverwaltung mit Github.
-                </p>
+                <section>
+                    <p>
+                        Ich heiße Etienne Silué und bin 1995 in Korhogo, einer Stadt im Norden der Elfenbeinküste (Côte d'Ivoire, Westafrika), geboren. Dort ging ich zur Schule und besuchte anschließend die Universität, an der ich mein Bachelor-Studium absolvierte.
+
+
+                    </p>
+                    <p>
+                        2021 heiratete ich und zog deshalb nach Deutschland. Ich wollte die Gelegenheit dieses Neubeginns nutzen, um die berufliche Karriere im Bereich meiner Träume zu starten: Web- und Softwareentwicklung.
+                    </p>
+                    <p>
+                        Die ersten Monate in Deutschland nutzte ich, um meine Deutsch-Kenntnise zu verbessern und freute mich, im Sommer 2022 eine Umschulung mit DCI (Digital Career Institute) beginnen zu können, um ein Web- und Softwareentwickler zu werden.
+                        Nun bin ich auf der Suche nach einer Arbeitsstelle und freue mich auf diese neue Herausforderung.
+                    </p>
+                    <p>
+                        Diese Fähigkeiten bringe ich bereits mit:
+
+                    </p>
+                    <p>
+                        - Frontend (UI/UX) design mit html, css, scss, bootstrap, Javascript, React js;
+                    </p>
+                    <p>
+                        - Backend mit Node.js, express js, mongodb(compass, Atlas)
+                    </p>
+                    <p>
+                        - MySql; Versionsverwaltung mit Github.
+                    </p>
+                </section>
             </section>
             <div className='btnContainer'>
 
