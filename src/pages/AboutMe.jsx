9866a5153b3db2f9972a37b7636c36a635189b48
Silte1 <etiennesilue3@gmail.com>
2023-05-30 08:26:43 +0200

    Commiit

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,12 +7,21 @@ import "./AboutMe.css"
 export default function AboutMe () {
     return (
         <article className='aboutMeContainer'>
-            <section className='textAboutMe'>Ich bin am Ende einer spannenden Umschulung zum fullstack Webentwickler bei DCI.
-                Gerade bin ich actif auf der Suche nach Arbeits-Praktikumsstelle ab Ende Juni.
-                Das biete ich an:
-                Frontend (UI/UX) design mit html, css, scss, bootstrap, Javascript, React js;
-                Backend mit Node.js, express js, mongodb(compass, Atlas) / MySql;
-                Versionsverwaltung mit Github.</section>
+            <section className='textAboutMe'>
+                <p>
+                    My name is Etienne Silué and I was born in Korhogo in 1995. Korhogo is a city in the northern Côte d'Ivoire, which lies in West Africa. I went to school in that city and did a Bachelor's degree after that.
+
+                    In 2021 I moved to Germany to be married. I wanted to use the opportunity of this new beginning to start my career in the field of activity of my dreams - web and software development.
+
+                    I used the past months to improve my German and was happy to be able to start the professional training with DCI to become a web and software developer.
+                </p>
+                <p>
+                    Das kann ich bereits:
+                    Frontend (UI/UX) design mit html, css, scss, bootstrap, Javascript, React js;
+                    Backend mit Node.js, express js, mongodb(compass, Atlas) / MySql;
+                    Versionsverwaltung mit Github.
+                </p>
+            </section>
             <div className='btnContainer'>
 
                 <button><a className='cv' href={resume} download="lebenslauf-etienne-silue">Lebenslauf</a></button>
